87a298c7c25ecabb00c1e1e3b2c162863f6e0382
Jaime Dias <jaimejosediasnt@gmail.com>
2025-05-14 02:58:53 -0300

    Verificacao do dominio

---
+++ zones/observatudo.com.br/domain-verification.tf
@@ -0,0 +1,8 @@
+resource "google_dns_record_set" "domain_verification" {
+  name         = "observatudo.com.br."
+  type         = "TXT"
+  ttl          = 300
+  managed_zone = google_dns_managed_zone.observatudo.name
+  project      = var.project_id
+  rrdatas      = ["\"google-site-verification=GDnV-hzuk6ywxz1URyEFdo8_Aess8gUxwrd7dwTpHuU\""]
+}
